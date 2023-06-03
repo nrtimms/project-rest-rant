f12389faeccbeef2a1533385967135622fb2189d
nrtimms <nataliertimms18@gmail.com>
2023-06-03 12:38:18 -0700

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -5,8 +5,14 @@ function error404 () {
     return (
       <Def>
           <main>
-              <h1>404: PAGE NOT FOUND</h1>
-              <p>Oops, sorry, we can't find this page!</p>
+            <h1>404: PAGE NOT FOUND</h1>
+            <p>Oops, sorry, we can't find this page!</p>
+            <div>
+                <img src="/images/error.jpg" alt="sad octopus plushie" width="500"/>
+                <div>
+                Photo by <a href="https://unsplash.com/@mahdibafande?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Mahdi Bafande</a> on <a href="https://unsplash.com/s/photos/error?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+            </div>
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
         <Def>
         <main>
             <h1>HOME</h1>
+            <div>
+                <img src="/images/tacos.jpg" alt="Tacos Al Pastor" width="500"/>
+                <div>
+                    Photo by <a href="https://unsplash.com/@jarritos?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jarritos Mexican Soda</a> on <a href="https://unsplash.com/photos/lmfEWru4V7A?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+            </div>
             <a href="/places">
                 <button className="btn-primary">Places Page</button>
             </a>
