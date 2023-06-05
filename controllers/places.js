5805bdb30db78cdc5c49d516326aaf7339a874fe
nrtimms <nataliertimms18@gmail.com>
2023-06-05 16:11:25 -0700

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
 
+router.post('/new', (req, res) => {
+    res.render('places/')
+  })
+
 router.get('/new', (req, res) => {
     res.render('places/new')
   })
