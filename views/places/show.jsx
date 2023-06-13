fd57b9df8ba21b93cc8b54ab09d629e449e2af2d
nrtimms <nataliertimms18@gmail.com>
2023-06-12 19:53:58 -0700

    added place data

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -10,7 +10,7 @@ function show (data) {
             <h2>Rating</h2>
             <p>not rated</p>
             <h2>Description</h2>
-            <p>Located in {data.place.city}, {data.place.state}. Serving {data.place.cuisines}</p>
+            <p>{data.place.showEstablished()}</p>
             <h2>Comments</h2>
             <p>no comments</p>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
