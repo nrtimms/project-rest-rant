2560251964b0978855b10bbdfebabe49e1cd4ba6
nrtimms <nataliertimms18@gmail.com>
2023-06-14 15:05:31 -0700

    added comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className="inactive">
+          No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+        return (
+            <div className="border">
+              <h2 className="rant">{c.rant ? 'Rant!' : 'Rave!'}</h2>
+              <h4>{c.content}</h4>
+              <h3>
+                <stong>- {c.author}</stong>
+              </h3>
+              <h4>Rating: {c.stars}</h4>
+            </div>
+        )
+        })
+    }
     return (
         <Def>
           <main>
@@ -12,7 +31,9 @@ function show (data) {
             <h2>Description</h2>
             <p>{data.place.showEstablished()}</p>
             <h2>Comments</h2>
-            <p>no comments</p>
+            <div className="row">
+              {comments}
+            </div>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                 Edit
             </a>     
