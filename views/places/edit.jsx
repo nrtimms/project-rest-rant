8ddae6e22e619d7f3f7ddb9a2a91689b8ec18d8d
nrtimms <nataliertimms18@gmail.com>
2023-06-18 11:49:08 -0700

    fixed edit and delete buttons

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,5 +1,5 @@
 const React = require('react')
-const Def = require('../default.jsx')
+const Def = require('../default')
 
 function edit_form (data) {
     return (
@@ -16,20 +16,26 @@ function edit_form (data) {
                         <label htmlFor="pic">Place Picture</label>
                         <input className="form-control" id="pic" name="pic" value={data.place.pic} />
                     </div>
-                    <div className="form-group col-sm-6">
+                </div>
+                <div className="row">
+                    <div className="form-group col-sm-4">
                         <label htmlFor="city">City</label>
                         <input className="form-control" id="city" name="city" value={data.place.city} />
                     </div>
-                    <div className="form-group col-sm-6">
+                    <div className="form-group col-sm-4">
                         <label htmlFor="state">State</label>
                         <input className="form-control" id="state" name="state" value={data.place.state} />
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="cuisines">Cuisines</label>
-                        <input className="form-control" id="cuisines" name="cuisines" value={data.place.cuisines} required />
+                    <div className="form-group col-sm-4">
+                        <label htmlFor="founded">Founding Year</label>
+                        <input className="form-control" id="founded" name="founded" value={data.place.founded} />
                     </div>
-                    <input className="btn btn-primary" type="submit" value="Add Place" />
                 </div>
+                <div className="form-group">
+                    <label htmlFor="cuisines">Cuisines</label>
+                    <input className="form-control" id="cuisines" name="cuisines" value={data.place.cuisines} required />
+                </div>
+                <input className="btn btn-primary" type="submit" value="Update Place" />
             </form>
           </main>
         </Def>
