b05fa93cd13b5bc5a3c67e21bd467f30f12eeda2
nrtimms <nataliertimms18@gmail.com>
2023-06-18 11:16:03 -0700

    displayed average rating

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,21 @@ function show (data) {
           No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+          Not rated yet
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+          }, 0)
+          let averageRating = sumRatings / data.place.comments.length
+          rating = (
+            <h3>
+              {Math.round(averageRating)} stars
+            </h3>
+          )
         comments = data.place.comments.map(c => {
         return (
             <div className="border">
@@ -27,7 +41,7 @@ function show (data) {
             <h1>{data.place.name}</h1>
             <img src={data.place.pic} alt={data.place.name} />
             <h2>Rating</h2>
-            <p>not rated</p>
+            {rating}
             <h2>Description</h2>
             <p>{data.place.showEstablished()}</p>
             <h2>Comments</h2>
